33d896071104344006968def0c2e9ee41413bb65
Leonardo <leonardo.guilherme@gmail.com>
2017-12-13 22:37:51 -0200

    feat(*): get rid of tcc files. implement some more stuff

--- examples/fun/fun.cc
+++ examples/fun/fun.cc
@@ -1,9 +1,10 @@
 #include "datetime.h"
 #include <iostream>
+#include <limits>
 
 int main(int argc, char * argv[]) {
-  asap::datetime x(0);
-  asap::duration d(123456789);
+  asap::datetime x;
+  asap::duration d(std::numeric_limits<uint64_t>::max());
   std::cout << x << std::endl;
   std::cout << d << std::endl;
   return 0;
--- include/datetime.h
+++ include/datetime.h
@@ -3,13 +3,14 @@
 
 #include <ctime>
 #include <string>
+#include <iomanip>
+#include <sstream>
 
 namespace asap {
   class duration;
   class datetime {
     public:
-      datetime() noexcept;
-      explicit datetime(time_t time) noexcept;
+      explicit datetime(time_t time = std::time(nullptr)) noexcept;
       datetime(const std::string & datetime, const std::string & format);
       datetime & operator+=(const duration & duration);
       datetime & operator-=(const duration & duration);
@@ -20,9 +21,29 @@ namespace asap {
     private:
       std::tm when;
   };
+
+
+  inline datetime::datetime(time_t time) noexcept : when{} {
+    when = *(std::localtime(&time));
+  }
+
+  inline datetime::datetime(const std::string & datetime, const std::string & format) : when{} {
+    std::istringstream ss(datetime);
+    ss >> std::get_time(&when, format.c_str());
+  }
+
+  inline time_t datetime::timestamp() const {
+    std::tm temp = when;
+    return mktime(&temp);
+  }
+
+  inline std::string datetime::str(const std::string & fmt) const {
+    char data[256];
+    std::strftime(data, sizeof(data), fmt.c_str(), &when);
+    return std::string(data);
+  }
 }
 
-#include "datetime.tcc"
 #include "operators.h"
 
 #endif // ASAP_DATETIME_H
