ed7d50e15c7ee0d6819bf37f8ee9e2fc64924fee
Leonardo <leonardo.guilherme@gmail.com>
2017-12-13 23:53:24 -0200

    feat(duration): add append method in detail ns

--- include/duration.h
+++ include/duration.h
@@ -6,6 +6,20 @@
 #include "constants.h"
 
 namespace asap {
+  namespace detail {
+    inline static unsigned long long
+    append(std::string & str, uint64_t seconds, uint64_t count, const std::string & singular,
+           const std::string & plural = "") {
+      long r = static_cast<unsigned>(seconds) / count;
+      if (!r) return seconds;
+      if (!str.empty()) str += ", ";
+      str += std::to_string(r) + " ";
+      if (r < 2) str += singular;
+      else str += plural.empty() ? singular + 's' : plural;
+      return static_cast<unsigned>(seconds) % count;
+    }
+  }
+
   class duration {
     public:
       explicit duration(uint64_t seconds);
@@ -15,7 +29,6 @@ namespace asap {
       duration & operator-=(long other);
 
       std::string str() const;
-
 
       float years() const { return transform(SECONDS_IN_YEAR); }
       float months() const { return transform(SECONDS_IN_MONTH); }
@@ -33,51 +46,42 @@ namespace asap {
       }
   };
 
-  duration::duration(uint64_t seconds) : seconds_(seconds) { }
+  inline duration::duration(uint64_t seconds)
+      : seconds_(seconds) { }
 
 
-  duration & duration::operator+=(const duration & other) {
+  inline duration & duration::operator+=(const duration & other) {
     return operator+=(other.seconds());
   }
 
-  duration & duration::operator-=(const duration & other) {
+  inline duration & duration::operator-=(const duration & other) {
     return operator-=(other.seconds());
   }
 
-  duration & duration::operator+=(long other) {
+  inline duration & duration::operator+=(long other) {
     seconds_ += other;
     return *this;
   }
 
-  duration & duration::operator-=(long other) {
+  inline duration & duration::operator-=(long other) {
     if (other > seconds_) seconds_ = 0;
     else seconds_ -= other;
     return *this;
   }
 
-  static unsigned long long append(std::string & str, uint64_t seconds, uint64_t count, const std::string & singular,
-                                   const std::string & plural = "") {
-    long r = static_cast<unsigned>(seconds) / count;
-    if (!r) return seconds;
-    if (!str.empty()) str += ", ";
-    str += std::to_string(r) + " ";
-    if (r < 2) str += singular;
-    else str += plural.empty() ? singular + 's' : plural;
-    return static_cast<unsigned>(seconds) % count;
-  }
-
   std::string duration::str() const {
     uint64_t seconds = seconds_;
-    std::string str; str.reserve(100);
+    std::string str;
+    str.reserve(100);
 
     // TODO: l10n i18n?
-    seconds = append(str, seconds, SECONDS_IN_YEAR, "year");
-    seconds = append(str, seconds, SECONDS_IN_MONTH, "month");
-    seconds = append(str, seconds, SECONDS_IN_WEEK, "week");
-    seconds = append(str, seconds, SECONDS_IN_DAY, "day");
-    seconds = append(str, seconds, SECONDS_IN_HOUR, "hour");
-    seconds = append(str, seconds, SECONDS_IN_MINUTE, "minute");
-    append(str, seconds, 1, "second");
+    seconds = detail::append(str, seconds, SECONDS_IN_YEAR, "year");
+    seconds = detail::append(str, seconds, SECONDS_IN_MONTH, "month");
+    seconds = detail::append(str, seconds, SECONDS_IN_WEEK, "week");
+    seconds = detail::append(str, seconds, SECONDS_IN_DAY, "day");
+    seconds = detail::append(str, seconds, SECONDS_IN_HOUR, "hour");
+    seconds = detail::append(str, seconds, SECONDS_IN_MINUTE, "minute");
+    detail::append(str, seconds, 1, "second");
 
     return str;
   }
