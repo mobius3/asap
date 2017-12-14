17a0898ba9bb39d5c4936512ecfb6b23563fdfd3
Leonardo <leonardo.guilherme@gmail.com>
2017-12-13 23:43:48 -0200

    feat(constants): fix constants, duh

--- include/constants.h
+++ include/constants.h
@@ -4,12 +4,12 @@
 #include <cstdint>
 
 namespace asap {
-  static constexpr uint64_t SECONDS_IN_YEAR = (60 * 60 * 24 * 365);
-  static constexpr uint64_t SECONDS_IN_MONTH = (60 * 60 * 24 * 365/12);
-  static constexpr uint64_t SECONDS_IN_WEEK = (60 * 60 * 24 * 365/12/4);
-  static constexpr uint64_t SECONDS_IN_DAY = (60 * 60 * 24);
-  static constexpr uint64_t SECONDS_IN_HOUR = (60 * 60);
-  static constexpr uint64_t SECONDS_IN_MINUTE = (60);
+  static constexpr uint64_t SECONDS_IN_MINUTE = 60;
+  static constexpr uint64_t SECONDS_IN_HOUR = SECONDS_IN_MINUTE * 60;
+  static constexpr uint64_t SECONDS_IN_DAY = SECONDS_IN_HOUR * 24;
+  static constexpr uint64_t SECONDS_IN_MONTH = SECONDS_IN_DAY * 30;
+  static constexpr uint64_t SECONDS_IN_YEAR = SECONDS_IN_DAY * 365;
+  static constexpr uint64_t SECONDS_IN_WEEK = SECONDS_IN_DAY * 7;
 }
 
 #endif //ASAP_CONSTANTS_H
