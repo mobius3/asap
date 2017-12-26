27457689b13a859b4d0f81a11d8c96378e7c8638
Leonardo <leonardo.guilherme@gmail.com>
2017-12-26 20:01:33 -0200

    feat(examples): enhace datetime example

--- examples/datetime/datetime.cc
+++ examples/datetime/datetime.cc
@@ -2,10 +2,28 @@
 #include "asap.h"
 
 int main(int argc, char * argv[]) {
+  /* Setting locale helps with local time format. It is not required. */
   setlocale(LC_ALL, "");
-  std::string s;
-  asap::datetime now;
-  asap::datetime then = now + asap::years(10);
-  std::cout << "now: " << now << std::endl;
-  std::cout << "then: " << then << std::endl;
+
+  /* If no arguments passed, its the same as asap::now() */
+  asap::datetime d1;
+
+  /* You can pass a string as a date or time. It tries its best to detect the format,
+   * it defaults to asap::datetime(0). */
+  asap::datetime d2("08/07/1987");
+
+  /* You can also pass a format specifier. They're the same used by strftime */
+  asap::datetime d3("08/07/1987 23:45:12");
+
+  /* A broken down time may be specified, in the order: year, month, day of month, hour, minute, second.
+   * Be aware that months start at 0 for January (as most date/time APIs) */
+  asap::datetime d4(1987, 6, 8);
+
+  /* Default printing format is day-month-year hour:minute:second */
+  std::cout << "d1: " << d1 << std::endl;
+
+  /* You can also pass a format to str(). Consult strftime manual for the available formats. */
+  std::cout << "d2: " << d2.str("%c") << std::endl;
+  std::cout << "d3: " << d3.str("date: %d/%m/%Y time: %H:%M") << std::endl;
+  std::cout << "d4: " << d4.str("%FT%T%z") << std::endl;
 }
