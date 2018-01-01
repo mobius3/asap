d60ed5dadbda09cadbcb5f189273a8690b942f6b
Leonardo <leonardo.guilherme@gmail.com>
2018-01-01 19:55:28 -0200

    add period example initial files

---
+++ examples/period/period.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "asap.h"
+
+int main(int argc, char * argv[]) {
+  /* Setting locale helps with local time format. It is not required. */
+  setlocale(LC_ALL, "");
+
+  return 0;
+}
