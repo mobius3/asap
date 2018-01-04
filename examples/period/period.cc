d4aa03d0267693109a3fe246f396ea1c95c0625d
Leonardo <leonardo.guilherme@gmail.com>
2018-01-04 01:21:16 -0200

    feat(examples): improve period example

--- examples/period/period.cc
+++ examples/period/period.cc
@@ -5,9 +5,23 @@ int main(int argc, char * argv[]) {
   /* Setting locale helps with local time format. It is not required. */
   setlocale(LC_ALL, "");
 
-  auto p = asap::now().until(asap::now() + asap::days(10));
+  /* Creates a period, from now until tomorrow. */
+  auto period = asap::now().until(asap::tomorrow());
 
-  for (auto d: p.every(asap::days(1))) {
+  /* Iterates from now until tomorrow, by every hour, using oldschool iterators.
+   * Be aware that tomorrow happens to be next day *exactly* at midnight, which means
+   * unless asap::now() is exactly at 0 minutes and 0 seconds, the next day will
+   * never be printed. */
+  auto everyhour = period.every(asap::hours(1));
+  std::cout << "ever hour until tomorrow:" << std::endl;
+  for (auto i = everyhour.begin(); i != everyhour.end(); i++) {
+    std::cout << *i << std::endl;
+  }
+  std::cout << std::endl;
+
+  /* you can also use the newschool for :) */
+  std::cout << "ever 3 hours until tomorrow:" << std::endl;
+  for (auto d: period.every(asap::hour(3))) {
     std::cout << d << std::endl;
   }
 
