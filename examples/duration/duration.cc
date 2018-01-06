d7bc3f3ecfea2830b719eaa84fed3305731c1f54
Leonardo <leonardo.guilherme@gmail.com>
2018-01-06 16:26:09 -0200

    fix(examples): remove unused code

--- examples/duration/duration.cc
+++ examples/duration/duration.cc
@@ -50,8 +50,5 @@ int main(int argc, char * argv[]) {
    * is actually 6 months, 2 days and 12 hours. asap::years(0.5) != asap::months(6) */
   std::cout << "half_year            ->   " << half_year << std::endl;
 
-  std::cout << "now          ->  " << asap::now() << std::endl;
-  std::cout << "now + 1 day  ->  " << asap::now() + asap::days(1) << std::endl;
-
   return 0;
 }
